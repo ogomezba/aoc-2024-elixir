2fb2e2060c26993197b45aea0ede1c0302170fc7
ogomezba <oscargomezbalaguer@gmail.com>
2024-12-05 17:05:17 +0100

    Day 5 of Advent of Code 2024. Part Two

--- lib/day5.ex
+++ lib/day5.ex
@@ -39,4 +39,47 @@ defmodule Day5 do
     |> Enum.map(&get_middle/1)
     |> Enum.sum()
   end
+
+  defp reorder(edges, pages) do
+    edges =
+      Enum.flat_map(edges, fn [a, b] ->
+        if a in pages do
+          [Graph.Edge.new(a, b)]
+        else
+          []
+        end
+      end)
+
+    Graph.new() |> Graph.add_edges(edges) |> Graph.topsort() |> Enum.reject(&(&1 not in pages))
+  end
+
+  def process_part_two() do
+    {manual, pages} =
+      "assets/input5.txt"
+      |> File.read!()
+      |> String.split("\n", trim: true)
+      |> Enum.split(1176)
+
+    manual_raw =
+      Enum.map(manual, fn line ->
+        [a, b] = String.split(line, "|", trim: true)
+        [String.to_integer(a), String.to_integer(b)]
+      end)
+
+    manual_grouped = Enum.group_by(manual_raw, fn [_, b] -> b end, fn [a, _] -> a end)
+
+    pages =
+      pages
+      |> Enum.map(fn row ->
+        row
+        |> String.split(",", trim: true)
+        |> Enum.map(&String.to_integer/1)
+      end)
+
+    pages
+    |> Enum.reject(&is_valid?(&1, manual_grouped))
+    |> Enum.map(&reorder(manual_raw, &1))
+    |> Enum.map(&get_middle/1)
+    |> Enum.sum()
+  end
 end
