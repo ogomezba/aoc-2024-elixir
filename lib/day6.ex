3408b792ff966cbdc67ae7b110ba771557fa5933
ogomezba <oscargomezbalaguer@gmail.com>
2024-12-06 12:23:25 +0100

    Day 6 of Advent of Code 2024. Part Two

--- lib/day6.ex
+++ lib/day6.ex
@@ -15,8 +15,8 @@ defmodule Day6 do
 
     a = fn i, j -> area[{i, j}] end
 
-    {initial_cords, _} = Enum.find(area, fn {_cords, value} -> value == "^" end)
-    initial_state = {initial_cords, @up}
+    {initial_coords, _} = Enum.find(area, fn {_coords, value} -> value == "^" end)
+    initial_state = {initial_coords, @up}
 
     visited = navigate(a, initial_state, MapSet.new())
     MapSet.size(visited)
@@ -38,13 +38,59 @@ defmodule Day6 do
     end
   end
 
-  defp navigate(a, {{i, j}, dir}, visited) do
-    visited = MapSet.put(visited, {i, j})
+  defp navigate(a, {coords, dir}, visited) do
+    visited = MapSet.put(visited, coords)
 
-    case look(a, {i, j}, dir) do
-      "#" -> navigate(a, {{i, j}, turn_right(dir)}, visited)
+    case look(a, coords, dir) do
+      "#" -> navigate(a, {coords, turn_right(dir)}, visited)
       "O" -> visited
-      _ -> navigate(a, {move({i, j}, dir), dir}, visited)
+      _ -> navigate(a, {move(coords, dir), dir}, visited)
+    end
+  end
+
+  def process_part_two() do
+    area =
+      "assets/input6.txt"
+      |> File.read!()
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.split(&1, "", trim: true))
+      |> add_borders()
+      |> transform_to_map()
+
+    {initial_coords, _} = Enum.find(area, fn {_coords, value} -> value == "^" end)
+    initial_state = {initial_coords, @up}
+
+    count_potential_loops(initial_state, area)
+  end
+
+  defp count_potential_loops(initial_state, area) do
+    candidate_obstacle_coords =
+      area
+      |> Map.filter(fn {_coords, value} -> value == "." end)
+      |> Map.keys()
+
+    candidate_obstacle_coords
+    |> Enum.filter(&is_loop?(area, &1, initial_state))
+    |> Enum.count()
+  end
+
+  defp is_loop?(area, obstacle_coords, initial_state) do
+    candidate_area = Map.put(area, obstacle_coords, "#")
+    a = fn i, j -> candidate_area[{i, j}] end
+    do_is_loop?(a, initial_state, MapSet.new())
+  end
+
+  defp do_is_loop?(a, {coords, dir} = state, visited) do
+    if MapSet.member?(visited, state) do
+      true
+    else
+      visited = MapSet.put(visited, state)
+
+      case look(a, coords, dir) do
+        "#" -> do_is_loop?(a, {coords, turn_right(dir)}, visited)
+        "O" -> false
+        _ -> do_is_loop?(a, {move(coords, dir), dir}, visited)
+      end
     end
   end
 
